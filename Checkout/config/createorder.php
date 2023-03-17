45406446d48e4ea67ae97fab5ba86691d0aabde4
Andrii Makarevych <makarony4@Andriis-MacBook-Pro.local>
2023-03-17 11:42:17 +0200

    Виправлено бінд запиту

--- Checkout/config/createorder.php
+++ Checkout/config/createorder.php
@@ -6,34 +6,46 @@ $city = $_POST['city'];
 $address = $_POST['address'];
 $number = $_POST['number'];
 
-
 $sql = "INSERT INTO orders (full_name, `number` , city, address) VALUES (?, ?, ?, ?)";
 $stmt = mysqli_prepare($connect, $sql);
 mysqli_stmt_bind_param($stmt, "siss", $full_name, $number, $city, $address);
 mysqli_stmt_execute($stmt);
 $last_order_id= (mysqli_insert_id($connect));
-mysqli_close($connect);
+
+$params = [];
+$types = [''];
+$query_products = "INSERT INTO order_products (product_id, count, product_title, product_price, order_id, total_price) values ";
+        if(!empty($_SESSION['cart'])) {
+        foreach ($_SESSION['cart'] as $key => $values) {
+            $query_products .= "(?,?,?,?,?,?),";
+            $order_id = $last_order_id;
+            $product_id = $values['id'];
+            $product_price = $values['price'];
+            $product_title = $values['title'];
+            $quantity = $values['quantity'];
+            $total_price = $quantity * $product_price;
+            $types[0] .= 'ssssii';
+
+            array_push($params, $product_id, $quantity, $product_title, $product_price, $order_id, $total_price);
+//            $values = array_values($values);
+//            $values[] = $last_order_id;
+//            $values[] = $values[2] * $values[3];
+//            $items[] = $values;
 
 
-
-if(!empty($_SESSION['cart'])) {
-    foreach ($_SESSION['cart'] as $key => $value) {
-        $product_id = $value['id'];
-        $quantity = $value['quantity'];
-        $product_title = $value['title'];
-        $product_price = $value['price'];
-        $total_price = $value ['quantity'] * $value['price'];
-
-//треба затрімити отримувані результати з форми , так як стмт дозволяє добавити лише один раз
-        $sql_products = "INSERT INTO order_products (order_id, product_id, count, product_title, product_price, total_price) values (?, ?, ?, ?, ?, ?)";
-        $stmt_products = mysqli_prepare($connect,$sql_products);
-        mysqli_stmt_bind_param($stmt_products, "iiisii", $last_order_id, $product_id, $quantity, $product_title, $product_price, $total_price);
+        }
+        }
+        $query_products = substr($query_products, 0, -1);
+        $stmt_products = mysqli_prepare($connect, $query_products);
+        mysqli_stmt_bind_param($stmt_products, "$types[0]", ...$params);
         mysqli_stmt_execute($stmt_products);
-        mysqli_close($connect);
-    }
-}
 
+
+
+mysqli_close($connect);
 //перенаправляємо на сторінку подяки, відміняємо сесію корзини
-header('Location: ../typage.php');
-unset($_SESSION['cart']);
+//header('Location: ../typage.php');
+//unset($_SESSION['cart']);
     ?>
+
+
