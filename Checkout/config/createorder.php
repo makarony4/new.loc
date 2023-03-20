b1fe7fb049e1e3571853b9583f4ef13aad200fe0
Andrii Makarevych <makarony4@Andriis-MacBook-Pro.local>
2023-03-20 14:18:18 +0200

    додано автоматичне додавання знаків питання при бінді параметрів

--- Checkout/config/createorder.php
+++ Checkout/config/createorder.php
@@ -14,38 +14,31 @@ $last_order_id= (mysqli_insert_id($connect));
 
 $params = [];
 $types = [''];
+$i = 0;
 $query_products = "INSERT INTO order_products (product_id, count, product_title, product_price, order_id, total_price) values ";
         if(!empty($_SESSION['cart'])) {
         foreach ($_SESSION['cart'] as $key => $values) {
-            $query_products .= "(?,?,?,?,?,?),";
-            $order_id = $last_order_id;
-            $product_id = $values['id'];
-            $product_price = $values['price'];
-            $product_title = $values['title'];
-            $quantity = $values['quantity'];
-            $total_price = $quantity * $product_price;
-            $types[0] .= 'ssssii';
-
-            array_push($params, $product_id, $quantity, $product_title, $product_price, $order_id, $total_price);
-//            $values = array_values($values);
-//            $values[] = $last_order_id;
-//            $values[] = $values[2] * $values[3];
-//            $items[] = $values;
-
-
+                $total_price = $values['price'] * $values['quantity'];
+                $types[0] .= "sssssi";
+                array_push($params, $values['id'], $values['quantity'], $values['title'], $values['price'], $last_order_id, $total_price);
+                $i++;
+                if ($i ==1){
+                    $binded_params ="(" . str_repeat("?,",count($params));
+                    $binded_params = substr($binded_params, 0 , -1);
+                    $binded_params .= "),";
+                }
+            $query_products .= $binded_params;
         }
         }
         $query_products = substr($query_products, 0, -1);
         $stmt_products = mysqli_prepare($connect, $query_products);
-        mysqli_stmt_bind_param($stmt_products, "$types[0]", ...$params);
+        mysqli_stmt_bind_param($stmt_products, $types[0], ...$params);
         mysqli_stmt_execute($stmt_products);
-
-
 
 mysqli_close($connect);
 //перенаправляємо на сторінку подяки, відміняємо сесію корзини
-//header('Location: ../typage.php');
-//unset($_SESSION['cart']);
+header('Location: ../typage.php');
+unset($_SESSION['cart']);
     ?>
 
 
