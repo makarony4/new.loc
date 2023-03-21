3a33fadd459b940e591631f1865c73f529b69b80
Andrii Makarevych <makarony4@Andriis-MacBook-Pro.local>
2023-03-21 20:05:10 +0200

    added functions

--- Checkout/config/createorder.php
+++ Checkout/config/createorder.php
@@ -2,48 +2,41 @@
 session_start();
 require_once ('../../connect.php');
 
-function saveProducts($params, $types){
-return $params;
-}
 
-function insertOrder($table_name, $columns, $bind, $types, $params){
+$delivery_info = [];
+$order_marks = '';
+$order_types = '';
+function deliveryInfo(array $arr){
+    global $delivery_info, $order_marks,$order_types;
+        $item = array_values($arr);
+        array_push($delivery_info, ...$item);
+        $order_marks .= "(" . str_repeat("?,", count($arr));
+        $order_marks = substr($order_marks, 0, -1);
+        $order_marks .= ")";
+        $order_types .= 'siss';
+}
+function insertOrder($table_name, $columns, $bind, $types, array $params){
     global $connect;
     $query = "INSERT INTO $table_name ($columns) VALUES $bind";
     $stmt_products = mysqli_prepare($connect, $query);
     mysqli_stmt_bind_param($stmt_products, "$types",...$params);
     mysqli_stmt_execute($stmt_products);
 }
-if(!$_POST){
-    foreach ($_POST as $item) {
-        echo $item;
-    }
-}
+deliveryInfo($_POST);
+var_dump(insertOrder('orders', 'full_name, city, address, number', $order_marks, $order_types, $delivery_info));
 
-//$full_name = $_POST['full_name'];
-//$city = $_POST['city'];
-//$address = $_POST['address'];
-//$number = $_POST['number'];
-//$sql = "INSERT INTO orders (full_name, `number` , city, address) VALUES (?, ?, ?, ?)";
-//$stmt = mysqli_prepare($connect, $sql);
-//mysqli_stmt_bind_param($stmt, "siss", $full_name, $number, $city, $address);
-//mysqli_stmt_execute($stmt);
-//$last_order_id= (mysqli_insert_id($connect));
-
-
-
-
+$last_order_id= (mysqli_insert_id($connect));
 $params = [];
 $types = '';
 $i = 0;
 $question_marks = '';
-//$query_products = "INSERT INTO order_products (product_id, product_title, product_price, count, order_id, total_price) values ";
-        if(!empty($_SESSION['cart'])) {
+if(!empty($_SESSION['cart'])) {
         foreach ($_SESSION['cart'] as $key => $values) {
                 $param = array_values($values);
                 array_push($params, ...$param);
                 $total_price = $values['price'] * $values['quantity'];
                 $types .= "ssssii";
-//                array_push($params, $last_order_id, $total_price);
+                array_push($params, $last_order_id, $total_price);
                 $i++;
                 if ($i ==1){
                     $binded_params ="(" . str_repeat("?,",count($params));
@@ -59,8 +52,9 @@ $question_marks = '';
 
 
 
-        $columns = "product_id, product_title, product_price, count, order_id, total_price";
-//        insertOrder('order_products',$columns , $question_marks, $types, $params);
+
+$columns = "product_id, product_title, product_price, count, order_id, total_price";
+insertOrder('order_products',$columns , $question_marks, $types, $params);
 //перенаправляємо на сторінку подяки, відміняємо сесію корзини
 //header('Location: ../typage.php');
 //unset($_SESSION['cart']);
