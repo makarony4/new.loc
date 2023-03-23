79fa07ca6f7ac1e583c05fd889e2ffcfaaa985e2
Andrii Makarevych <makarony4@Andriis-MacBook-Pro.local>
2023-03-23 12:22:09 +0200

    added working functions

--- Checkout/config/createorder.php
+++ Checkout/config/createorder.php
@@ -2,44 +2,34 @@
 session_start();
 require_once ('../../connect.php');
 require_once ('../../funcs/funcs.php');
-
-deliveryInfo($_POST);
-insertOrder('orders', 'full_name, city, address, number', $order_marks, $order_types, $delivery_info);
-
-var_dump(array_values($_POST));
 $last_order_id= (mysqli_insert_id($connect));
-$params = [];
-$types = '';
-$i = 0;
-$question_marks = '';
-if(!empty($_SESSION['cart'])) {
-        foreach ($_SESSION['cart'] as $key => $values) {
-                $param = array_values($values);
-                array_push($params, ...$param);
-                $total_price = $values['price'] * $values['quantity'];
-                $types .= "ssssii";
-                array_push($params, $last_order_id, $total_price);
-                $i++;
-                if ($i ==1){
-                    $binded_params ="(" . str_repeat("?,",count($params));
-                    $binded_params = substr($binded_params, 0 , -1);
-                    $binded_params .= "),";
-                }
-            $question_marks .= $binded_params;
-        }
-        }
-        $question_marks = substr($question_marks, 0, -1);
+
+$columns = '';
+foreach ($_POST as $key => $value){
+$columns .= $key . ",";
+}
+$columns = substr($columns, 0 , -1);
+
+insertOrder('orders',$columns , getMarks(deliveryInfo($_POST)), getTypes($_POST), deliveryInfo($_POST));
+$last_order_id= (mysqli_insert_id($connect));
+
+$columns = '';
+foreach($_SESSION['cart'] as $keys => $values){
+    foreach ($values as $key  => $value){
+        $columns .= $key . ",";
+    }
+    break;
+}
+$products_column = "order_id, " . $columns . " total_price";
 
 
 
+insertOrder('order_products', $products_column, getMarks($_SESSION['cart']), getTypes(getParams($_SESSION['cart'])), (getParams($_SESSION['cart'])));
 
 
-
-$columns = "product_id, product_title, product_price, count, order_id, total_price";
-insertOrder('order_products',$columns , $question_marks, $types, $params);
 //перенаправляємо на сторінку подяки, відміняємо сесію корзини
-//header('Location: ../typage.php');
-//unset($_SESSION['cart']);
+header('Location: ../typage.php');
+unset($_SESSION['cart']);
     ?>
 
 
--- funcs/funcs.php
+++ funcs/funcs.php
@@ -1,23 +1,65 @@
 <?php
-//беремо інфу з форми замовлення
-$delivery_info = [];
-$order_marks = '';
-$order_types = '';
-function deliveryInfo(array $arr){
-    global $delivery_info, $order_marks,$order_types;
-    $item = array_values($arr);
-    array_push($delivery_info, ...$item);
-    $order_marks .= "(" . str_repeat("?,", count($arr));
-    $order_marks = substr($order_marks, 0, -1);
-    $order_marks .= ")";
-    $order_types .= 'ssss';
+//витягуємо параметри
+function getParams(array $arr){
+    $params = [];
+    global $last_order_id;
+    foreach ($arr as $key => $values) {
+        $param = array_values($values);
+        $total_price = $values['price'] * $values['quantity'];
+        array_push($params,$last_order_id, ...$param);
+        $params[] = $total_price;
+
+    }
+    return $params;
 }
 
+
+//беремо інфу з форми замовлення
+function deliveryInfo(array $arr){
+    $delivery_info = [];
+    $item = array_values($arr);
+    array_push($delivery_info, ...$item);
+    return $delivery_info;
+}
+
+
+//ловимо знаки питання для бінду
+function getMarks($count){
+    $question_mark = "(" . str_repeat('?,' , count($count));
+    if ($count == $_SESSION['cart']){
+        $question_mark = "(" . str_repeat('?,' , count($_SESSION['cart'][0]) + 2);
+    }
+    $question_mark = substr($question_mark, 0, -1);
+    $question_mark .= ")";
+    if ($count == $_SESSION['cart']){
+$question_mark .= ',';
+$question_mark = str_repeat($question_mark, count($_SESSION['cart']));
+$question_mark = substr($question_mark, 0 ,-1);
+    }
+    return $question_mark;
+}
+
+
+//ловимо типи даних для бінду
+
+function getTypes($params){
+    $type = '';
+    foreach ($params as $param){
+        if(is_string($param)){
+        $type .= 's';
+        }
+        elseif(is_int($param)){
+            $type .= 'i';
+        }
+    }
+return $type;
+}
 
 //вставляємо в мускл
 function insertOrder($table_name, $columns, $bind, $types, array $params){
     global $connect;
     $query = "INSERT INTO $table_name ($columns) VALUES $bind";
     $stmt_products = mysqli_prepare($connect, $query);
-    mysqli_stmt_bind_param($stmt_products, "$types",...$params);
-    mysqli_stmt_execute($stmt_products);}
+    mysqli_stmt_bind_param($stmt_products, $types ,...$params);
+    mysqli_stmt_execute($stmt_products);
+}
